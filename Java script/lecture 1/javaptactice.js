383cd15937d2ed9b494a0b7b6becf7737dab4c29
kamleshworkspace777 <kamleshworkspace@gmail.com>
2025-01-17 13:59:51 +0530

    commit 1

---
+++ Java script/lecture 1/javaptactice.js
@@ -0,0 +1,25 @@
+
+//odd or even
+let num = 10;
+if (num %2 ===0) {
+    console.log(num, "num is even")
+}
+else {
+    console.log(num, "num is odd")
+}
+//Else-if conditional statement
+let mode = "pink";
+let color;50
+if (mode === "dark") {
+    color = "black";
+} else if (mode === "pink") {
+    color = "pink";
+} else if (mode === "blue") {
+    color = "blue";
+} else {
+    color = "white";
+}
+console.log(color);
+let number = prompt("hello!");
+let result = number / 5?"yes":"no";
+console.log(result);
---
+++ Java script/lecture 2/practice.js
@@ -0,0 +1,17 @@
+
+let marks = prompt("Enter your score");
+if (marks >= 80){
+    console.log("Grade A");
+}
+else if (marks >= 70 && marks <=89){
+    console.log("Grade B");
+}
+else if (marks >= 60 && marks <=79){
+    console.log("Grade C");
+}
+else if (marks >= 50 && marks <=69){
+    console.log("Grade D");
+}
+else{
+    console.log("Grade F");
+}
